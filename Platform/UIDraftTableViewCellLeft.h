4584511943de2aa56d0ab373680eccf2ba44ef71
Jordan Gurrieri <Jordang29@hotmail.com>
2011-11-05 12:04:34 -0700

    some draftviewcontroller stuff

---
+++ Platform/UIDraftTableViewCellLeft.h
@@ -0,0 +1,16 @@
+//
+//  UIDraftTableViewCellLeft.h
+//  Platform
+//
+//  Created by Jordan Gurrieri on 11/4/11.
+//  Copyright 2011 Blue Label Solutions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface UIDraftTableViewCellLeft : UITableViewCell {
+    
+}
+
+@end
---
+++ Platform/UIDraftTableViewCellLeft.m
@@ -0,0 +1,35 @@
+//
+//  UIDraftTableViewCellLeft.m
+//  Platform
+//
+//  Created by Jordan Gurrieri on 11/4/11.
+//  Copyright 2011 Blue Label Solutions LLC. All rights reserved.
+//
+
+#import "UIDraftTableViewCellLeft.h"
+
+
+@implementation UIDraftTableViewCellLeft
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
