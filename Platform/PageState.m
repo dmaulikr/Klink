d8ae5f5497c0be23d4c02388ccb3caf4e3b822c9
Bobby Gill <bobby.gill@gmail.com>
2011-11-06 11:56:51 -0500

    including refresh header

---
+++ Platform/PageState.m
@@ -0,0 +1,14 @@
+//
+//  PageState.m
+//  Platform
+//
+//  Created by Bobby Gill on 11/5/11.
+//  Copyright 2011 Blue Label Solutions LLC. All rights reserved.
+//
+
+#import "PageState.h"
+
+
+@implementation PageState
+
+@end
