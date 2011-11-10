ca50a4abc459b60f37ea667c2ea697af5570b413
Jordan Gurrieri <Jordang29@hotmail.com>
2011-11-10 00:42:22 -0800

    dicks

---
+++ Platform/FullScreenPhotoViewControllerOLD.h
@@ -0,0 +1,16 @@
+//
+//  FullScreenPhotoViewControllerOLD.h
+//  Platform
+//
+//  Created by Jordan Gurrieri on 11/7/11.
+//  Copyright 2011 Blue Label Solutions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FullScreenPhotoViewControllerOLD : FullScreenPhotoViewControllerOLD {
+    
+}
+
+@end
