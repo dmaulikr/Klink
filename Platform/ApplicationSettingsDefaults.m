21897126714e22025d79169f8417ec0cffb8c998
Bobby Gill <bobby.gill@gmail.com>
2011-12-07 14:30:12 -0500

    daa

--- Platform/ApplicationSettingsDefaults.m
+++ Platform/ApplicationSettingsDefaults.m
@@ -16,14 +16,14 @@
 //NSString* const default_BASEURL = @"http://192.168.1.102/bonobo";
 
 //this URL is to connect to JORDAN's development environment over the internet
-NSString* const default_BASEURL = @"http://216.243.43.184/bonobo";
+//NSString* const default_BASEURL = @"http://216.243.43.184/bonobo";
 
 //this URL is to connect to JORDAN's development environment from his internal network
 //NSString* const default_BASEURL = @"http://192.168.1.4/bonobo";
 
 
 //this URL is to connect to the PRODUCTION environment in the cloud
-//NSString* const default_BASEURL = @"http://oscial.com/bonobo";
+NSString* const default_BASEURL = @"http://oscial.com/bonobo";
 
 
 
