fbf1cf5ebcf8c3cdf0d994bb338f95fc9afcce21
Jordan Gurrieri <jordang29@hotmail.com>
2012-02-21 18:44:04 -0500

    Updated some text in the alerts.

--- Platform/UIStrings.m
+++ Platform/UIStrings.m
@@ -17,22 +17,22 @@
 
  NSString* const ui_EMPTY_CAPTION = @" ";
 
- NSString* const ui_WELCOME_PRODUCTIONLOG       = @"Each page in Bahndr starts here as a Draft.\n\nEvery night, Bahndr Editors vote for one Draft to publish in Bahndr.\n\nGlory comes to those who are published, will you be glorious?\n\nLet's find out, press the center key to create your first Draft...";
+ NSString* const ui_WELCOME_PRODUCTIONLOG       = @"Each page in Bahndr starts here as a Draft.\n\nEvery night, Bahndr Editors vote for one Draft to publish in Bahndr.\n\nGlory comes to those who are published, will you be glorious?\n\nLet's find out, press the center typewriter key to create your first Draft...";
 
 
  NSString* const ui_WELCOME_BOOK                = @"Each day, one new story is written into the Bahndr memory book.\n\nYou create the content, you choose what gets published, you write the Bahndr story.\n\nDismiss this alert, and swipe the page to kick start your Bahndr-ing...";
 
  NSString* const ui_WELCOME_FULLSCREEN          = @"Use this view to browse through the photos and captions in this Draft.\n\nAdd your own photos and captions to this draft with the toolbar.\n\nRemember to use the 'thumb up' button to up-vote your favorite entries.";
 
- NSString* const ui_WELCOME_DRAFT               = @"A Draft consists of a hashtag title and any number of photos and captions.\n\nBefore a Draft reaches its deadline, any Bahndrer can add to it, or up-vote their favorite entries to the top.\n\nAt the deadline, only the highest voted photo and caption is considered for publishing.";
+ NSString* const ui_WELCOME_DRAFT               = @"A Draft consists of a hashtag title and any number of photos and captions.\n\nBefore a Draft reaches its deadline, any Bahndrer can add their own photo or caption to it, or up-vote their favorite entries to the top.\n\nAt the deadline, only the highest voted photo and caption is considered for publishing.";
 
- NSString* const ui_WELCOME_PROFILE             = @"Welcome! Use the Profile screen to track your Bahndr activity. Contribute enough drafts, photos and captions in one week and you'll be promoted to Editor.\n\nRemember, while any Bahndrer can write, its the Editors that decide what is published.";
+ NSString* const ui_WELCOME_PROFILE             = @"Welcome! Use the Profile screen to track your Bahndr activity. Contribute enough drafts, photos and captions each week and you'll be promoted to Editor.\n\nRemember, while any Bahndrer can write, its the Editors that decide what is published.";
 
  NSString* const ui_WELCOME_EDITORIAL           = @"You've made it, you are a Bahndr Editor! Only Editors decide what Draft will be published next.\n\nGo on, cast your vote for your favorite Draft. Remember Bahndr is forever, guard it well.";
 
-NSString* const ui_LOGIN_REQUIRED = @"While anyone can read Bahndr, only logged-in Bahndrers can write it.\n\nJoining Bahndr is easy, just login with your Facebook credentials and you are done!";
+NSString* const ui_LOGIN_REQUIRED               = @"While anyone can read Bahndr, only logged-in Bahndrers can contribute to it.\n\nJoining Bahndr is easy, just login with Facebook and you are done!\n\nWe use Facebook because it is far more secure for you than anything we could build.";
 
-NSString* const ui_LOGIN_TITLE = @"Join Bahndr";
+NSString* const ui_LOGIN_TITLE                  = @"Join Bahndr";
 
 
-NSString* const ui_WELCOME_CONTRIBUTE = @"Drafts have 3 parts: a hashtag, photo and caption. You can start a Draft with just a hashtag title. Anyone can then add a photo or caption to the Draft before it expires.\n\nGo on, let that Great Dane named creativity off her chain and start writing this Draft..."; 
+NSString* const ui_WELCOME_CONTRIBUTE           = @"Drafts have 3 parts: a hashtag, photo and caption. You can start a Draft with just a hashtag title. Anyone can then add a photo or caption to the Draft before it expires.\n\nGo on, let that Great Dane named creativity off her chain and start writing this Draft..."; 
