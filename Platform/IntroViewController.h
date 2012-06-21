18faf138b1bef186dec7fe1aaf425737e1295793
NuttySimple <MacbookPro@Jordans-MacBook-Pro.local>
2012-06-21 11:45:54 -0700

    Commit

---
+++ Platform/IntroViewController.h
@@ -0,0 +1,13 @@
+//
+//  IntroViewController.h
+//  Platform
+//
+//  Created by Jordan Gurrieri on 6/20/12.
+//  Copyright (c) 2012 Blue Label Solutions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntroViewController : UIViewController
+
+@end
---
+++ Platform/IntroViewController.m
@@ -0,0 +1,44 @@
+//
+//  IntroViewController.m
+//  Platform
+//
+//  Created by Jordan Gurrieri on 6/20/12.
+//  Copyright (c) 2012 Blue Label Solutions LLC. All rights reserved.
+//
+
+#import "IntroViewController.h"
+
+@interface IntroViewController ()
+
+@end
+
+@implementation IntroViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
